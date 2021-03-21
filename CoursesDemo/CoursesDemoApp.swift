24ac79e36083d563d5c094019c96799e3b639706
Tejas M R <matblockgreninja@gmail.com>
2021-03-21 15:19:19 +0530

    Initial Commit

---
+++ CoursesDemo/CoursesDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoursesDemoApp.swift
+//  CoursesDemo
+//
+//  Created by tezz on 21/03/21.
+//
+
+import SwiftUI
+
+@main
+struct CoursesDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
